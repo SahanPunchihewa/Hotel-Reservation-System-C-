3b58c687b973980f7e21fbabef524e388cbde2d8
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-01-26 17:09:54 +0530

    Complete

--- Hotel Reservation System/AddRoom.Designer.cs
+++ Hotel Reservation System/AddRoom.Designer.cs
@@ -29,12 +29,261 @@ namespace Hotel_Reservation_System
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddRoom));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_type = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Rtype = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Rnumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_amount = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(653, 1);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(145, 97);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 33;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(608, 378);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 46);
+            this.button4.TabIndex = 51;
+            this.button4.Text = "Delete Room";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(453, 378);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 46);
+            this.button3.TabIndex = 50;
+            this.button3.Text = "Update Room";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(12, 378);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 46);
+            this.button6.TabIndex = 49;
+            this.button6.Text = "<< Back";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SpringGreen;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(150, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 46);
+            this.button2.TabIndex = 48;
+            this.button2.Text = "View Room";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(302, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 46);
+            this.button1.TabIndex = 46;
+            this.button1.Text = "Add Room";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_type
+            // 
+            this.txt_type.Location = new System.Drawing.Point(302, 295);
+            this.txt_type.Name = "txt_type";
+            this.txt_type.Size = new System.Drawing.Size(206, 20);
+            this.txt_type.TabIndex = 45;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(132, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "AC or Non AC";
+            // 
+            // txt_Rtype
+            // 
+            this.txt_Rtype.Location = new System.Drawing.Point(302, 241);
+            this.txt_Rtype.Name = "txt_Rtype";
+            this.txt_Rtype.Size = new System.Drawing.Size(206, 20);
+            this.txt_Rtype.TabIndex = 43;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(132, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 18);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Room Type (S or L)";
+            // 
+            // txt_Rnumber
+            // 
+            this.txt_Rnumber.Location = new System.Drawing.Point(302, 188);
+            this.txt_Rnumber.Name = "txt_Rnumber";
+            this.txt_Rnumber.Size = new System.Drawing.Size(206, 20);
+            this.txt_Rnumber.TabIndex = 41;
+            this.txt_Rnumber.TextChanged += new System.EventHandler(this.txt_Rnumber_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(132, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 18);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Room Number";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(302, 134);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(206, 20);
+            this.txt_id.TabIndex = 39;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Room ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(327, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 32);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Add Room";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(132, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 18);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "Room Amount";
+            // 
+            // txt_amount
+            // 
+            this.txt_amount.Location = new System.Drawing.Point(302, 337);
+            this.txt_amount.Name = "txt_amount";
+            this.txt_amount.Size = new System.Drawing.Size(206, 20);
+            this.txt_amount.TabIndex = 53;
+            // 
+            // AddRoom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_amount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_type);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Rtype);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_Rnumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "AddRoom";
             this.Text = "AddRoom";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_type;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Rtype;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Rnumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_amount;
     }
 }
--- Hotel Reservation System/AddRoom.cs
+++ Hotel Reservation System/AddRoom.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Hotel_Reservation_System
 {
@@ -15,6 +16,149 @@ namespace Hotel_Reservation_System
         public AddRoom()
         {
             InitializeComponent();
+        }
+
+        private void txt_Rnumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection = "server=localhost; userId=root; password=; database=hotelsystem";
+
+                string query = "INSERT INTO `room` (`id`, `Rnumber`, `Rtype`, `type`, `amount`) VALUES ('" + this.txt_id.Text + "', '" + this.txt_Rnumber.Text + "', '" + this.txt_Rtype.Text + "', '" + this.txt_type.Text + "', '"+this.txt_amount+"');";
+                MySqlConnection Myconn = new MySqlConnection(MyConnection);
+                MySqlCommand Mycommand = new MySqlCommand(query, Myconn);
+                MySqlDataReader MyReader;
+                Myconn.Open();
+                MyReader = Mycommand.ExecuteReader();
+                MessageBox.Show("Room Details Added Successful..!!!");
+
+                txt_id.Text = "";
+                txt_Rnumber.Text = "";
+                txt_Rtype.Text = "";
+                txt_type.Text = "";
+                txt_amount.Text = "";
+
+
+                Myconn.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (txt_id.Text != "")
+                {
+
+                    string MyConnection = "server=localhost; userId=root; password=; database=hotelsystem";
+                    string query = "SELECT * FROM `room` WHERE id= '" + txt_id.Text + "'";
+                    MySqlConnection Myconn = new MySqlConnection(MyConnection);
+                    MySqlCommand Mycommand = new MySqlCommand(query, Myconn);
+                    MySqlDataReader MyReader;
+                    Myconn.Open();
+                    MyReader = Mycommand.ExecuteReader();
+                    Myconn.Close();
+                    MySqlDataAdapter adp = new MySqlDataAdapter(Mycommand);
+                    DataTable dt = new DataTable();
+                    adp.Fill(dt);
+
+                    txt_Rnumber.Text = dt.Rows[0][1].ToString();
+                    txt_Rtype.Text = dt.Rows[0][2].ToString();
+                    txt_type.Text = dt.Rows[0][3].ToString();
+                    txt_amount.Text = dt.Rows[0][4].ToString();
+
+                }
+                else
+                {
+                    MessageBox.Show("Enter Value for ID");
+                }
+
+            }
+            catch
+            {
+
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection = "server=localhost; userId=root; password=; database=hotelsystem";
+
+                string query = "UPDATE `room` SET `id` = '" + this.txt_id.Text + "', `Rnumber` = '" + this.txt_Rnumber.Text + "', `Rtype` ='" + this.txt_Rtype.Text + "', `type`= '" + this.txt_type.Text + "', `amount` = '"+this.txt_amount+"' WHERE `id`= '" + this.txt_id.Text + "'";
+                MySqlConnection Myconn = new MySqlConnection(MyConnection);
+                MySqlCommand Mycommand = new MySqlCommand(query, Myconn);
+                MySqlDataReader MyReader;
+                Myconn.Open();
+                MyReader = Mycommand.ExecuteReader();
+                MessageBox.Show("Room Details Successfully Updated!");
+                while (MyReader.Read())
+
+                txt_id.Text = "";
+                txt_Rnumber.Text = "";
+                txt_Rtype.Text = "";
+                txt_type.Text = "";
+                txt_amount.Text = "";
+
+
+                Myconn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection = "server=localhost; userId=root; password=; database=hotelsystem";
+                string query = "DELETE FROM `room` WHERE id = '" + this.txt_id.Text + "'";
+
+                MySqlConnection Myconn = new MySqlConnection(MyConnection);
+                MySqlCommand Mycommand = new MySqlCommand(query, Myconn);
+                MySqlDataReader MyReader;
+                Myconn.Open();
+                MyReader = Mycommand.ExecuteReader();
+                MessageBox.Show("Room Successfully Deleted!");
+
+                txt_id.Text = "";
+                txt_Rnumber.Text = "";
+                txt_Rtype.Text = "";
+                txt_type.Text = "";
+                txt_amount.Text = "";
+
+                Myconn.Close();
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
